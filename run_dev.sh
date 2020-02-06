f6ab1e10cdc777d05c32abfc7ee2ab79a2428e72
George Lifchits <george.lifchits@gmail.com>
2020-02-06 15:21:16 -0500

    github pages

--- run_dev.sh
+++ run_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.8
-docker run --name hccblog --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
+docker run --rm --name hccblog --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
